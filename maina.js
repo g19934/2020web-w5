7703a66e9deb26d5bdad015ed65b8b1bfdd1aea1
g19934 <75404840+g19934@users.noreply.github.com>
2021-01-07 10:59:15 +0900

    Update maina.js

--- maina.js
+++ maina.js
@@ -8,9 +8,9 @@ function randomValueFromArray(array){
 }
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let insertX = ['寂しい','偉大なる父','るんるんホリデー'];
+let insertY = ['ハッピーハウス','ふわふわ島','くるるん道路'];
+let insertZ = ['困惑した','妖怪のせいで転んだ','早く家に帰りましょう。'];
 
 randomize.addEventListener('click', result);
 
