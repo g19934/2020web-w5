f44b3e023e26b10a30312575270cf48495771c1b
g19934 <75404840+g19934@users.noreply.github.com>
2021-01-07 18:12:06 +0900

    Update maina.js

--- maina.js
+++ maina.js
@@ -31,11 +31,11 @@ function result() {
     newStory = newStory.replace('Bob',name);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
-    newStory = newStory.replace('300 pounds',weight);
+  if(document.getElementById("日本").checked) {
+    const weight = Math.round(300*0.0714286) + ' キロ';
+    const temperature =  Math.round((94-32) * 5 / 9) + '度';
+    newStory = newStory.replace('94 度',temperature);
+    newStory = newStory.replace('300 ポンド',weight);
   }
 
   story.textContent = newStory;
